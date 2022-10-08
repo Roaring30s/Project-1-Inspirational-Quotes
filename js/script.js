efaec15965d7455b3ff011301d3ab8ac193b404a
Roaring30s <okaaycrypto@gmail.com>
2022-10-07 21:30:55 -0500

    Add Changing Color Scheme on Quote Change
    A changing color scheme was added to quotes object array. A while loop that tracks lastIndex to prevent repeated quotes was added. A vanilla JS fade-in effect was added on quotes changing.

--- js/script.js
+++ js/script.js
@@ -4,6 +4,12 @@ project 1 - A Random Quote Generator
 ******************************************/
 
 /*** 
+ * `lastIndex`
+ * Tracker assuring the same quote is not repeated continuously
+***/
+let lastIndex = 0;
+
+/*** 
  * `quotes` array 
 ***/
 const quotes = [
@@ -11,37 +17,43 @@ const quotes = [
     quote: "The way of success is the way of continuous pursuit of knowledge.",
     source: "Napoleon Hill",
     citation: "Think and Grow Rich",
-    year: "1937"
+    year: "1937",
+    colorScheme: ["#7bf240", "#2cd9bc"]
   },
   {
     quote: "An investment in knowledge pays the best interest.",
     source: "Benjamin Franklin",
     citation: "",
-    year: ""
+    year: "",
+    colorScheme: ["#F8B195", "#F67280"]
   },
   {
     quote: "Learn as if you will live forever, live like you will die tomorrow.",
     source: "Mahatma Gandhi",
     citation: "",
-    year: ""
+    year: "",
+    colorScheme: ["#EC2049", "#F26B38"]
   },
   {
     quote: "Success is not final; failure is not fatal: It is the courage to continue that counts.",
     source: "Winston Churchill",
     citation: "",
-    year: ""
+    year: "",
+    colorScheme: ["#F9CDAD", "#C8C8A9"]
   },
   {
-    quote: "Nothing in the world can take the place of Persistence. Talent will not; nothing is more common than unsuccessful men with talent. Genius will not; unrewarded genius is almost a proverb. Education will not; the world is full of educated derelicts. The slogan 'Press On' has solved and always will solve the problems of the human race.",
+    quote: "Nothing in the world can take the place of Persistence.",
     source: "Calvin Coolidge",
     citation: "The Dallas Morning News",
-    year: "1929"
+    year: "1929",
+    colorScheme: ["#45ADA8", "#547980"]
   },
   {
     quote: "If you are working on something that you really care about, you don’t have to be pushed. The vision pulls you.",
     source: "Steve Jobs",
     citation: "",
-    year: ""
+    year: "",
+    colorScheme: ["#A7226E", "#F7DB4F"]
   }
 ];
 
@@ -51,14 +63,17 @@ const quotes = [
  * @returns {array} quote - returns object from quotes variable
  */
 function getRandomQuote() {
-  const randomNum = Math.floor(Math.random() * quotes.length);
+  let randomNum = Math.floor(Math.random() * quotes.length);
+  while (lastIndex === randomNum) { //Prevent randomNum and lastIndex matching
+    randomNum = Math.floor(Math.random() * quotes.length);
+  }
+  lastIndex = randomNum;
   return quotes[randomNum];
 }
 
 /**
  * `printQuote`
  * Builds HTML string to display random quote on browser
- * @returns {array} quote - returns object from quotes variable
  */
 
 function printQuote() {
@@ -75,6 +90,28 @@ function printQuote() {
   }
   htmlStr += `</p>`;
   document.getElementById('quote-box').innerHTML = htmlStr;
+  const color = quoteObj.colorScheme;
+  document.body.style.backgroundImage = `linear-gradient(100deg,${color[0]},${color[1]},${color[0]},${color[1]})`;
+  fadeIn(document.getElementById('quote-box'), 2000);
+}
+
+/**
+ * `fadeIn`
+ * Enables a fade-in effect for html element
+ * @source https://codepen.io/jorgemaiden/pen/xoRKWN
+ */
+function fadeIn(element, duration = 600) {
+  element.style.display = '';
+  element.style.opacity = 0;
+  var last = +new Date();
+  var tick = function () {
+    element.style.opacity = +element.style.opacity + (new Date() - last) / duration;
+    last = +new Date();
+    if (+element.style.opacity < 1) {
+      (window.requestAnimationFrame && requestAnimationFrame(tick)) || setTimeout(tick, 16);
+    }
+  };
+  tick();
 }
 
 /*** 
@@ -86,3 +123,6 @@ document.getElementById('load-quote').addEventListener("click", printQuote, fals
  *  SET INTERVAL
 ***/
 setInterval(printQuote, 5000);
+
+
+
